3c8e659ce8545cb062b92a1d7341c09d31a9ef80
Adam Dehnel <arsdehnel@gmail.com>
2025-05-10 09:46:45 -0500

    feat: converted to nextjs

---
+++ src/components/about.js
@@ -0,0 +1,97 @@
+export default function About() {
+    return (
+        <>
+            <div id="aboutBack">
+
+                <div class="sectionStart" id="about"></div>
+
+                <div>
+
+                    <div id="aboutSection">
+
+                        <div class="aboutBox">
+                            <div id="aboutCopy">
+                                <h3>about me</h3>
+                                <p>As a dynamic UX/UI designer and creative director, I pride myself in providing smart, meaningful, and unique design that solves problems and adds value to the human experience.</p><p>With over twenty years of experience in both the advertising and publishing industries, I am excited to share my passion and expertise as a <span class="bold">user champion</span>, <span class="bold">creative partner</span>, and <span class="bold">brand expert</span>.</p>
+                            </div>
+                        </div>
+
+                        <div id="aboutSkill">
+                            <span>
+                                <div id="scrollHead">
+                                    <img class="scrollIcon" src="images/DT-carrot-grayDown50.svg" />
+                                    <h6>scroll skill set</h6>
+                                </div>
+                                <div id="vertScroll">
+                                    <h5>Empathy</h5>
+                                    <h5>A/B Testing</h5>
+                                    <h5>Affinity Diagrams</h5>
+                                    <h5>Card Sorting</h5>
+                                    <h5>Task Analysis</h5>
+                                    <h5>User Interview Planning</h5>
+                                    <h5>User Interviews</h5>
+                                    <h5>Usability Testing</h5>
+                                    <h5>Copywriting</h5>
+                                    <h5>Information Architecture</h5>
+                                    <h5>Creativity</h5>
+                                    <h5>Collaboration</h5>
+                                    <h5>Site Mapping</h5>
+                                    <h5>Wireframing</h5>
+                                    <h5>HTML</h5>
+                                    <h5>CSS</h5>
+                                    <h5>Bootstrap</h5>
+                                    <h5>Page Layout</h5>
+                                    <h5>Mockups</h5>
+                                    <h5>Sketching</h5>
+                                    <h5>Prototyping</h5>
+                                    <h5>UI Design</h5>
+                                    <h5>Branding</h5>
+                                    <h5>Color Theory</h5>
+                                    <h5>Gestalt Principles</h5>
+                                    <h5>Icon Design</h5>
+                                    <h5>Infographics</h5>
+                                    <h5>Data Visualization</h5>
+                                    <h5>Layout Design</h5>
+                                    <h5>Typography</h5>
+                                    <h5>Project Management</h5>
+                                </div>
+                            </span>
+                        </div>
+
+                    </div>
+
+
+                    <div id="aboutGrid">
+
+                        <div class="aboutGridCopy" id="user">
+                            <h6>user empathy + user flows</h6>
+                            <p>I encompass and understand user goals for all projects in order to create deliverables that provide exceptional user experience, information discovery, and delight.</p>
+                        </div>
+                        <div class="aboutGridCopy" id="content">
+                            <h6>content + user interface design</h6>
+                            <p>With strong organizational skills, I build information architecture and navigation designed to serve, then use creative content and layouts to bring messaging to life.</p><p>Beautifully crafted, branded, and comprehensive UI give the user a complete experience.</p>
+                        </div>
+                        <div class="aboutGridCopy" id="project">
+                            <h6>project management + collaboration</h6>
+                            <p>Following all aspects of a project keep expectations and deadlines met.</p><p>Because each project is unique, I collaborate with industry experts to produce exceptional work while keeping direction aligned and deliverables cohesive.</p>
+                        </div>
+                        <div class="aboutGridCopy" id="toolbox">
+                            <h6>toolbox</h6>
+                            <ul>
+                                <li>Listening</li>
+                                <li>Good ol&apos; Pencil & Paper</li>
+                                <li>Adobe Creative Cloud: XD, Illustrator, InDesign, Photoshop</li>
+                                <li>Figma + Figjam</li>
+                                <li>Jira + Trello</li>
+                                <li>Miro</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
