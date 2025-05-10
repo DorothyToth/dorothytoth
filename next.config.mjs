be3222010660654fd2333da375ca572c56aba7e7
Adam Dehnel <arsdehnel@gmail.com>
2025-05-10 18:31:27 -0500

    ci: disable next optimization setting in workflow

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,16 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: process.env.CI ? "/dorothytoth-site" : "",
-  distDir: 'out'
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
