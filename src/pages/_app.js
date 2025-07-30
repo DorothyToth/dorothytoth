8223e73c1dbcb0c8c71960706ee06e7aaead051f
Dorothy Toth <dorothytoth@me.com>
2025-07-30 18:41:03 -0500

    less robot-y words, more me words

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>Dorothy Toth&apos;s Website</title>
+				<title>Dorothy Toth</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<Layout>
