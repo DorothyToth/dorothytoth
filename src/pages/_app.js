a9667ffa42c4243a90f61d21eb2e2d7032f12b79
Dorothy Toth <dorothytoth@me.com>
2025-07-30 17:28:18 -0500

    bookmark words

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>Dorothy Toth&apos;s Portfolio</title>
+				<title>Dorothy Toth&apos;s Website</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<Layout>
