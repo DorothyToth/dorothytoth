7cde2f95c82bd2480dc6887d06c343ec907f100b
Dorothy Toth <dorothytoth@me.com>
2025-07-30 17:18:06 -0500

    bookmark name update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>Dorothy&apos;s UX/UI Portfolio</title>
+				<title>Dorothy Toth&apos;s Portfolio</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<Layout>
