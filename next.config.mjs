6d07542a480d3375414ba7efd05b5540c05fcd01
Adam Dehnel <arsdehnel@gmail.com>
2025-08-16 08:45:21 -0500

    ci: adjust build path for PR reviews

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   output: "export",
 
   /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+
+  basePath: process.env.PR_NUMBER ? `/pr-preview/pr-${ process.env.PR_NUMBER }/` : "",
+
+  /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
