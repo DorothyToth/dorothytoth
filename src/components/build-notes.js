84aff24edf0b91408c454537804f1496d4f499c1
Adam Dehnel <arsdehnel@gmail.com>
2025-08-16 17:32:42 -0500

    ci: build vars are only inlined with direct var reference

--- src/components/build-notes.js
+++ src/components/build-notes.js
@@ -1,24 +1,10 @@
 export default function BuildNotes() {
-
-    const envVarKeys = [
-        'GITHUB_SHA',
-        'GITHUB_RUN_ID',
-        'REPO_UPDATED_AT',
-        'GITHUB_REF_NAME',
-        'NEXT_PUBLIC_GITHUB_SHA',
-        'NEXT_PUBLIC_GITHUB_RUN_ID',
-        'NEXT_PUBLIC_REPO_UPDATED_AT',
-        'NEXT_PUBLIC_GITHUB_REF_NAME',
-    ]
-
-
     return (
         <ul className="build-notes">
-            {
-                envVarKeys.map( key => {
-                    return <li key={ key }>{ key }: { process.env[ key ] }</li>
-                })
-            }
+            <li>NEXT_PUBLIC_GITHUB_SHA: { process.env.NEXT_PUBLIC_GITHUB_SHA }</li>
+            <li>NEXT_PUBLIC_GITHUB_RUN_ID: { process.env.NEXT_PUBLIC_GITHUB_RUN_ID }</li>
+            <li>NEXT_PUBLIC_REPO_UPDATED_AT: { process.env.NEXT_PUBLIC_REPO_UPDATED_AT }</li>
+            <li>NEXT_PUBLIC_GITHUB_REF_NAME: { process.env.NEXT_PUBLIC_GITHUB_REF_NAME }</li>
         </ul>
     )
 }
