3b0eefad7bc03693371d7177bf962bb88887ce9e
Dorothy Toth <dorothytoth@me.com>
2025-12-06 17:11:49 -0600

    project page styling

--- src/pages/project-page.js
+++ src/pages/project-page.js
@@ -5,19 +5,29 @@ export default function projectPage({ projects }) {
 			<div id="project-page">
 
                 <div className="proj-margin">
-
-					<a href="/projects"><p>back</p></a>
+					<a href="/projects"><p>all projects</p></a>
 
                     <h1>Title of Project</h1>
 
                     <h3>subtitle</h3>
 
-                     <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
                      
-                     <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                    <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
 
                     <img src="/images/card-hike.jpeg" alt="alt text here" className="proj-image" />
+                </div>
 
+                <img src="/images/hero-hiking.jpg" alt="alt text here" className="proj-image-full"/>
+
+                <div className="proj-margin">
+                    <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                     
+                    <p>Lorem ipsum dolor sit amet, <span className="bold">consectetur adipiscing</span> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                </div>
+
+                <div className="proj-highlight">
+                    <p className="highlight-text">Lorem ipsum dolor sit amet, <b>consectetur adipiscing</b> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
                 </div>
 
             </div>
