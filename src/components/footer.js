40823f40bd84649466cdf4cb03feeaf397e70e27
Dorothy Toth <dorothytoth@me.com>
2026-02-27 19:24:41 -0600

    resume link to this directory

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ export default function Footer() {
                 <div id="links">
                     <a href="mailto:dorothytoth@me.com"><h5>Email</h5></a>
                     <a href="https://www.linkedin.com/in/dorothy-toth/" target="_blank"><h5>LinkedIn</h5></a>
-                    <a href="https://drive.google.com/file/d/1mlE8bfHJMiPIeZJE9e2bhgbLpriBsd1g/view?usp=sharing" target="_blank"><h5>Resume</h5></a>
+                    <a href="/DorothyToth-Resume.pdf" target="_blank"><h5>Resume</h5></a>
                 </div>
 
             </section>
